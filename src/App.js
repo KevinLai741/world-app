40c361d58c4b711f008cb9c9deaa69b3a7b041a1
Kevin Lai <kevin.lai@gmail.com>
2023-01-13 16:35:14 -0800

    wip in nav.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+// import TimeViewer from './components/TimeViewer';
+// import CurrencyConverter from './components/CurrencyConverter';
+// import WeatherViewer from './components/WeatherViewer';
+// import LocaleNews from './components/LocaleNews';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> World App </h1>
+          <Nav />
+          {/* <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/time" element={<TimeViewer/>} />
+            <CurrencyConverter />
+            <WeatherViewer />
+            <LocaleNews />
+          </Routes> */}
       </header>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,17 @@
-function NavigationTool () {
+import React, { useState } from 'react';
 
+function Nav() {
+    return(
+        <div class="topnav">
+        <li><a href="#">Top</a></li>
+        <li><a href="#convert">Convert</a></li>
+        <li><a href="#time">Time</a></li>
+        <li><a href="weather">Weather</a></li>
+        <li><a href="#news">News</a></li>
+    </div>
+    )
 }
 
 
 
-
-
-export default NavigationTool;
+export default Nav;
