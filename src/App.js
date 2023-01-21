e3b40654ba587546ce65288352fb58698740c4b3
Kevin Lai <kevin.lai@gmail.com>
2023-01-20 16:14:59 -0800

    recreating navigation bar tool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
-import TimeViewer from './components/TimeViewer';
-import CurrencyConverter from './components/CurrencyConverter';
-import WeatherViewer from './components/WeatherViewer';
-import LocaleNews from './components/LocaleNews';
+import Navbar from './components/NavBar/Navbar';
+// import Nav from './components/Nav';
+// import TimeViewer from './components/TimeViewer';
+// import CurrencyConverter from './components/CurrencyConverter';
+// import WeatherViewer from './components/WeatherViewer';
+// import LocaleNews from './components/LocaleNews';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> World App </h1>
-          <Nav />
-          <TimeViewer />
-          <CurrencyConverter />
-          <WeatherViewer />
-          <LocaleNews />
+        <Navbar />
+        {/* <TimeViewer />
+        <CurrencyConverter />
+        <WeatherViewer />
+        <LocaleNews /> */}
           {/* <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/time" element={<TimeViewer/>} />
@@ -25,7 +24,6 @@ function App() {
             <Route path="/weather" element={<WeatherViewer/>} />
             <Route path="/news" element={<LocaleNews/>} />
           </Routes> */}
-      </header>
     </div>
   );
 }
---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Currency Converter',
+        url: '#currency',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Time Viewer',
+        url: '#time',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Weather Viewer',
+        url: '#weather',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Locale News',
+        url: '#news',
+        cName: 'nav-links'
+    },
+]
